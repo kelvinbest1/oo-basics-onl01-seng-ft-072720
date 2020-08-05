6396c443e03db417d79d3348397c3f84675331f3
kelvin lanier <kelvinlanier@yahoo.com>
2020-08-05 01:39:13 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,24 +2,4 @@
 
 An error occurred while loading spec_helper.
 Failure/Error: require_relative '../lib/dog'
-
-SyntaxError:
-  /home/gregarious-editor-4373/ruby-object-attributes-lab-onl01-seng-ft-072720/lib/dog.rb:2: syntax error, unexpected '\n', expecting '.' or ::
-  /home/gregarious-editor-4373/ruby-object-attributes-lab-onl01-seng-ft-072720/lib/dog.rb:12: syntax error, unexpected end, expecting end-of-input
-# ./spec/spec_helper.rb:1:in `require_relative'
-# ./spec/spec_helper.rb:1:in `<top (required)>'
-
-An error occurred while loading ./spec/ruby_object_attributes_spec.rb.
-Failure/Error: require_relative '../lib/dog'
-
-SyntaxError:
-  /home/gregarious-editor-4373/ruby-object-attributes-lab-onl01-seng-ft-072720/lib/dog.rb:2: syntax error, unexpected '\n', expecting '.' or ::
-  /home/gregarious-editor-4373/ruby-object-attributes-lab-onl01-seng-ft-072720/lib/dog.rb:12: syntax error, unexpected end, expecting end-of-input
-# ./spec/spec_helper.rb:1:in `require_relative'
-# ./spec/spec_helper.rb:1:in `<top (required)>'
-# ./spec/ruby_object_attributes_spec.rb:1:in `require'
-# ./spec/ruby_object_attributes_spec.rb:1:in `<top (required)>'
-373/ruby-object-attributes-lab-onl01-seng-ft-072720/lib/dog.rb:12: syntax error, unexpected end, expecting end-of-input
-# ./spec/spec_helper.rb:1:in `require_relative'
-# ./spec/spec_hel.13331 seconds to load)
-0 examples, 0 failures, 2 errors occurred outside of examples
+outside of examples
